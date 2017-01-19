721cbdd16dbfa2b11449504f365f26e0d2232ca5
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-19 16:45:23 +0900

    fix jump page process

--- research_blogging/crawler.py
+++ research_blogging/crawler.py
@@ -40,7 +40,8 @@ class ResearchBloggingCrawler:
         self.before_url = url
         soup = self._make_soup(self.target_url)
         ul_pageBrowser = soup.find("ul", {"class": "pageBrowser"})
-        a_next = ul_pageBrowser.find("a", {"class": "underlined"})
+        a_nexts = ul_pageBrowser.find_all("a", {"class": "underlined"})
+        a_next = a_nexts[1]
 
         if a_next is not None and "href" in a_next.attrs:
             next_page_url = a_next["href"]
